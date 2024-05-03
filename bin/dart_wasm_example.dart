ffedda826e8f132b0f557cb6ce5cabea6d88dbb8
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2024-05-03 21:13:41 +0200

    Create project

---
+++ bin/dart_wasm_example.dart
@@ -0,0 +1,5 @@
+import 'package:dart_wasm_example/dart_wasm_example.dart' as dart_wasm_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_wasm_example.calculate()}!');
+}
---
+++ test/dart_wasm_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_wasm_example/dart_wasm_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
